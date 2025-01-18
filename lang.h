d37d98fb20a756b3703d2c68274765f792a43944
Artem Goryanov <73838119+artgor261@users.noreply.github.com>
2025-01-18 17:28:36 +1000

    Add files via upload

---
+++ lang.h
@@ -0,0 +1,21 @@
+int com(int num_1, int num_2);
+int coml(int num, int prev);
+int add(int num_1, int num_2);
+int addl(int num, int prev);
+int sb(int num_1, int num_2);
+int sbl(int num, int prev);
+int rp(int i);
+
+const int COUNT = 6;
+
+const struct {
+    char *name;
+    int (*func)(int, int);
+} func_map[] = {
+    {"add", &add},
+    {"addl", &addl},
+    {"sb", &sb},
+    {"sbl", &sbl},
+    {"com", &com},
+    {"coml", &coml}
+};
